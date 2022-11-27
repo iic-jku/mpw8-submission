e9185536fffb8377d6b9963b4a3e5f624102708a
Harald Pretl <harald@pretl.eu>
2022-11-27 14:17:45 +0100

    Revert "Delete make.sh"
    This reverts commit 3265e4ef20cfb174ce7967af030972dfb60a42a3.

---
+++ make.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+export PDK=sky130A
+export PDKPATH="$PDK_ROOT/$PDK"
+
+call_flow () {
+	echo "---------------------------------------------------"
+	echo "Hardening $1..."
+	echo "---------------------------------------------------"
+	flow.tcl -design "openlane/$1" -tag foo -overwrite
+}
+
+clean_result () {
+	echo "---------------------------------------------------"
+	echo "Deleting old results..."
+	echo "---------------------------------------------------"
+	[ "$(ls -A def)" ] 			&& rm def/*
+	[ "$(ls -A lef)" ] 			&& rm lef/*
+	[ "$(ls -A gds)" ] 			&& rm gds/*
+	[ "$(ls -A mag)" ] 			&& rm mag/*
+	[ "$(ls -A maglef)" ] 		&& rm maglef/*
+	[ "$(ls -A verilog/gl)" ] 	&& rm verilog/gl/*
+}
+
+stage_result () {
+	echo "---------------------------------------------------"
+	echo "Staging results of $1..."
+	echo "---------------------------------------------------"
+	cp "openlane/$1/runs/foo/results/final/def/$1.def" def
+	cp "openlane/$1/runs/foo/results/final/gds/$1.gds" gds
+	cp "openlane/$1/runs/foo/results/final/lef/$1.lef" lef
+	cp "openlane/$1/runs/foo/results/final/mag/$1.mag" mag
+	cp "openlane/$1/runs/foo/results/final/maglef/$1.mag" maglef
+	cp "openlane/$1/runs/foo/results/final/verilog/gl/$1.v" verilog/gl
+}
+
+# Get rid of old results
+#clean_result
+
+# Harden macros
+#call_flow audiodac
+#call_flow tempsense
+#call_flow config_reg
+
+# Stage results of hardening
+stage_result audiodac
+stage_result tempsense
+stage_result config_reg
+
+# Assemble top-level
+call_flow user_project_wrapper
+stage_result user_project_wrapper
