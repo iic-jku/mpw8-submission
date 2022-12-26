a4b49c9b9cff0689c0831afd31b8562a4b3d3ce8
Harald Pretl <harald@pretl.eu>
2022-12-26 19:54:40 +0100

    Improve usage of shared config refs, add linting comments, use new tie1/0 from config block

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -28,7 +28,8 @@
  *
  *-------------------------------------------------------------
  */
-
+//`define USE_POWER_PINS
+//`define SIM
 `include "defines.v"
 `include "config_reg_mux.v"
 `include "audiodac.v"
@@ -38,7 +39,9 @@
 //`include "/foss/pdks/sky130A/libs.ref/sky130_fd_sc_hd/verilog/primitives.v"
 
 module user_project_wrapper #(
+// verilator lint_off UNUSEDPARAM
     parameter BITS = 32
+// verilator lint_on UNUSEDPARAM
 ) (
 `ifdef USE_POWER_PINS
     inout vdda1,	// User area 1 3.3V supply
@@ -52,6 +55,7 @@ module user_project_wrapper #(
 `endif
 
     // Wishbone Slave ports (WB MI A)
+    // verilator lint_off UNUSEDSIGNAL
     input wb_clk_i,
     input wb_rst_i,
     input wbs_stb_i,
@@ -60,16 +64,21 @@ module user_project_wrapper #(
     input [3:0] wbs_sel_i,
     input [31:0] wbs_dat_i,
     input [31:0] wbs_adr_i,
+    // verilator lint_on UNUSEDSIGNAL
     output wbs_ack_o,
     output [31:0] wbs_dat_o,
 
     // Logic Analyzer Signals
+// verilator lint_off UNUSEDSIGNAL
     input  [127:0] la_data_in,
-    output [127:0] la_data_out,
     input  [127:0] la_oenb,
+// verilator lint_on UNUSEDSIGNAL
+    output [127:0] la_data_out,
 
     // IOs
+// verilator lint_off UNUSEDSIGNAL
     input  [`MPRJ_IO_PADS-1:0] io_in,
+// verilator lint_on UNUSEDSIGNAL
     output [`MPRJ_IO_PADS-1:0] io_out,
     output [`MPRJ_IO_PADS-1:0] io_oeb,
 
@@ -80,7 +89,9 @@ module user_project_wrapper #(
     inout [`MPRJ_IO_PADS-10:0] analog_io,
 
     // Independent clock (on independent integer divider)
+// verilator lint_off UNUSEDSIGNAL
     input   user_clock2,
+// verilator lint_on UNUSEDSIGNAL
 
     // User maskable interrupt signals
     output [2:0] user_irq
@@ -100,17 +111,22 @@ module user_project_wrapper #(
     wire        config_wr_w =       io_in[26];
     wire [1:0]  config_adr_w =      io_in[28:27];
     wire        trigger_in_w =      io_in[29];
+    assign      io_oeb[29:0] =      {30{tie_hi}};
 
     // ----------------------------
     // connect analog inputs to ADC
     // ----------------------------
     wire        adc_analog_in_p =   analog_io[30-7];
     wire        adc_analog_in_n =   analog_io[31-7];
+    assign      io_oeb[31:30] =     {2{tie_hi}};
 
     // -----------------------
     // connect outputs to core
     // -----------------------
+    assign      io_out[31:0] =      {32{reg0_b14}};
     assign      io_out[37:32] =     mux_out_w;
+    assign      io_oeb[37:32] =     {6{tie_lo}};
+    assign      {wbs_ack_o, wbs_dat_o, user_irq} = {36{reg0_b15}};
 
     // ---------------------------------
     // define wires for interconnections
@@ -119,15 +135,25 @@ module user_project_wrapper #(
     wire [5:0]  temp_dac_w, temp0_dac_w, temp1_dac_w, temp2_dac_w, temp3_dac_w;
     wire [11:0] temp_tick_w, temp0_tick_w, temp1_tick_w, temp2_tick_w, temp3_tick_w;
     wire        temp0_done_w, temp1_done_w, temp2_done_w, temp3_done_w;
-    wire [5:0]  adac_out_w;
+    wire [5:0]  dac_out_w;
     wire [15:0] reg0_w;
+    wire        reg0_b0 = reg0_w[0];
+    wire [3:0]  reg0_b4_b1 = reg0_w[4:1];
+    wire [1:0]  reg0_b6_b5 = reg0_w[6:5];
+    wire [1:0]  reg0_b8_b7 = reg0_w[8:7];
+    wire        reg0_b14 = reg0_w[14];
+    wire        reg0_b15 = reg0_w[15];
     wire [15:0] reg1_w;
+    wire        reg1_b0 = reg1_w[0];
+    wire        reg1_b1 = reg1_w[1];
+    wire [5:0]  reg1_b7_b2 = reg1_w[7:2];
     wire [15:0] reg2_w;
     wire [15:0] reg3_w;
-    wire        dummy1_w;
-    wire [1:0]  dummy2_w;
+    wire [15:0] dummy_adc_w;
     wire [15:0] adc_result_w;
     wire        adc_done_w;
+    wire        tie_hi;
+    wire        tie_lo;
 
     // -----------------------------------------------------------
     // connect LA signals just in case to monitor internal signals
@@ -136,7 +162,7 @@ module user_project_wrapper #(
     assign la_data_out[75:64] = {temp0_dac_w, temp_dac_w};
     assign la_data_out[99:76] = {temp0_tick_w, temp_tick_w};
     assign la_data_out[103:100] = {temp3_done_w, temp2_done_w, temp1_done_w, temp0_done_w};
-    assign la_data_out[109:104] = {adac_out_w};
+    assign la_data_out[109:104] = {dac_out_w};
     assign la_data_out[125:110] = {adc_result_w};
     assign la_data_out[126] = {adc_done_w};
 
@@ -154,22 +180,34 @@ module user_project_wrapper #(
         .reg_wr_i(config_wr_w),
         .reg_adr_i(config_adr_w),
         .reg_dat_i(data_in_w),
-        .reg0_o(reg0_w), .reg1_o(reg1_w), .reg2_o(reg2_w), .reg3_o(reg3_w),
+        .reg0_o(reg0_w),
+        .reg1_o(reg1_w),
+        .reg2_o(reg2_w),
+        .reg3_o(reg3_w),
         .mux_adr_i(data_out_sel_w),
         .mux_o(mux_out_w),
-        .mux0_i({reg0_w[15], adc_done_w, temp3_done_w, temp2_done_w, temp1_done_w, temp0_done_w}),
+        .mux0_i({tie_lo, adc_done_w, temp3_done_w, temp2_done_w, temp1_done_w, temp0_done_w}),
         .mux1_i(temp_dac_w),
         .mux2_i(temp_tick_w[5:0]),
         .mux3_i(temp_tick_w[11:6]),
-        .mux4_i(adac_out_w),
+        .mux4_i(dac_out_w),
         .mux5_i(adc_result_w[5:0]),
         .mux6_i(adc_result_w[11:6]),
-        .mux7_i({reg0_w[14:13], adc_result_w[15:12]}),
-        .temp_sel_i(reg0_w[8:7]),
-        .temp0_dac_i(temp0_dac_w), .temp1_dac_i(temp1_dac_w), .temp2_dac_i(temp2_dac_w), .temp3_dac_i(temp3_dac_w),
+        .mux7_i({tie_lo, tie_lo, adc_result_w[15:12]}),
+        .temp_sel_i(reg0_b8_b7),
+        .temp0_dac_i(temp0_dac_w),
+        .temp1_dac_i(temp1_dac_w),
+        .temp2_dac_i(temp2_dac_w),
+        .temp3_dac_i(temp3_dac_w),
         .temp_dac_o(temp_dac_w),
-        .temp0_ticks_i(temp0_tick_w), .temp1_ticks_i(temp1_tick_w), .temp2_ticks_i(temp2_tick_w), .temp3_ticks_i(temp3_tick_w),
+        .temp0_ticks_i(temp0_tick_w),
+        .temp1_ticks_i(temp1_tick_w),
+        .temp2_ticks_i(temp2_tick_w),
+        .temp3_ticks_i(temp3_tick_w),
         .temp_ticks_o(temp_tick_w),
+
+        .tie_hi(tie_hi),
+        .tie_lo(tie_lo),
 
         .loopback_i(la_data_in[127]),
         .loopback_o(la_data_out[127])
@@ -236,7 +274,9 @@ module user_project_wrapper #(
     // the digital audio dac from an earlier mpw
     // -----------------------------------------
 
-    audiodac adac0 (
+    assign dac_out_w[5] = tie_lo; 
+
+    audiodac dac0 (
     `ifdef USE_POWER_PINS
         .vccd1(vccd1),	// User area 1 1.8V power
         .vssd1(vssd1),	// User area 1 digital ground
@@ -244,19 +284,19 @@ module user_project_wrapper #(
 
         .fifo_i(data_in_w),
         .fifo_rdy_i(trigger_in_w),
-        .fifo_ack_o(adac_out_w[2]),
-        .fifo_full_o(adac_out_w[3]),
-        .fifo_empty_o(adac_out_w[4]),
+        .fifo_ack_o(dac_out_w[2]),
+        .fifo_full_o(dac_out_w[3]),
+        .fifo_empty_o(dac_out_w[4]),
         .rst_n_i(reset_n_w),
         .clk_i(clk_w),
-        .mode_i(reg0_w[0]),
-        .volume_i(reg0_w[4:1]),
-        .osr_i(reg0_w[6:5]),
-        .ds_o(adac_out_w[0]),
-        .ds_n_o(adac_out_w[1]),
-        .tst_fifo_loop_i(reg1_w[0]),
-        .tst_sinegen_en_i(reg1_w[1]),
-        .tst_sinegen_step_i(reg1_w[7:2])
+        .mode_i(reg0_b0),
+        .volume_i(reg0_b4_b1),
+        .osr_i(reg0_b6_b5),
+        .ds_o(dac_out_w[0]),
+        .ds_n_o(dac_out_w[1]),
+        .tst_fifo_loop_i(reg1_b0),
+        .tst_sinegen_en_i(reg1_b1),
+        .tst_sinegen_step_i(reg1_b7_b2)
     );
 
     // ---------------------------------------------------------
@@ -277,7 +317,8 @@ module user_project_wrapper #(
         .config_1_in(reg2_w),
         .config_2_in(reg3_w),
         .result_out(adc_result_w),
-        .conversion_finished_out(adc_done_w)
+        .conversion_finished_out(adc_done_w),
+        .dummypin(dummy_adc_w)
     );
 
 endmodule	// user_project_wrapper
